8cf6f656e46de126492fe44be428ea58d7c9ecd9
Sebastian <sebastian.chang23@gmail.com>
2020-10-29 20:45:37 -0500

    added sign in

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -61,7 +61,7 @@ const SignIn = () => {
                 />
                 <div className='buttons'>
                     <CustomButton type='submit'>Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                 </div>
             </form>
         </div>
