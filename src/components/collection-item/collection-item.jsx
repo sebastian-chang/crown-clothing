3245f5cb353965a2b8d836d39e7fe40d1c9bba9c
Sebastian <sebastian.chang23@gmail.com>
2020-11-04 14:51:06 -0600

    Added cart util to add item to cart on same line.  Updated shop data.

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
+import CustomButton from '../custom-button/custom-button'
+import { addItem } from '../../redux/cart/cart-actions'
 import './collection-item.styles.scss'
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+const CollectionItem = ({ item }) => {
+    const { name, price, imageUrl } = item
+    const dispatch = useDispatch()
+
+    const addToCart = item => {
+        dispatch(addItem(item))
+    }
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton onClick={() => addToCart(item)} inverted>Add to cart</CustomButton>
         </div>
-    </div>
-)
+    )
+}
+
+// const mapDispatchToProps = dispatch => ({
+//     addItem: item => dispatch(addItem(item))
+// })
+
+// export default connect(null, mapDispatchToProps)(CollectionItem)
 
 export default CollectionItem
