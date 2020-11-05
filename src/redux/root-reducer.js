37bba3d4340f150508496d9c826c252b7f2638b2
Sebastian <sebastian.chang23@gmail.com>
2020-11-04 21:12:04 -0600

    Added redux-persist for local storage of items in cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+// This is for local storage
+import storage from 'redux-persist/lib/storage'
+// This is for session storage
+// import sessionStorage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user-reducer'
 import cartReducer from './cart/cart-reducer'
 
-export default combineReducers({
+const persistConfig = {
+    // Where in our reducers do we want to start storing
+    key: 'root',
+    // Storage as the key and the storage where were saving
+    storage,
+    // List the reducers we would like to store to local storage by key name
+    whitelist: ['cart']
+}
+
+// Combine all reducers
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 })
+
+// Modified version of our rootReducer now with persisting capabilities
+export default persistReducer(persistConfig, rootReducer)
