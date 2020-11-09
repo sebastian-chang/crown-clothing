09df1adaf799dfa785cb54bd00dc9219c38f7785
Sebastian <sebastian.chang23@gmail.com>
2020-11-09 16:55:00 -0600

    Updating stripe intergration and updating our facicon and title

--- src/components/checkout/checkout.jsx
+++ src/components/checkout/checkout.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 
 import './checkout.styles.scss'
 import CheckoutItem from '../checkout-item/checkout-item'
+import StripeCheckoutButton from '../stripe-button/stripe-button'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selectors'
 
 const CheckoutPage = () => {
@@ -35,6 +36,12 @@ const CheckoutPage = () => {
       <div className='total'>
         <span>TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        42424 4242 4242 4242 - Exp: 12/24 - CVV 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   )
 }
