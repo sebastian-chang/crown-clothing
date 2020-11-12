94cf5797aef1a2cb8cb5a877c0732233208a0077
Sebastian <sebastian.chang23@gmail.com>
2020-11-12 15:24:10 -0600

    Finished Redux-Thunk

--- src/components/pages/shop/shop.jsx
+++ src/components/pages/shop/shop.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Route } from 'react-router-dom'
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../../firebase/firebase.utils'
-import { fetchCollectionsStartAsync, updateCollection } from '../../../redux/shop/shop-actions'
+// import { firestore, convertCollectionsSnapshotToMap } from '../../../firebase/firebase.utils'
+import { fetchCollectionsStartAsync } from '../../../redux/shop/shop-actions'
 
 import CollectionsOverview from '../../collections-overview/collections-overview'
 import CollectionPage from '../collection/collection'
@@ -14,30 +14,29 @@ const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionsPageWithSpinner = WithSpinner(CollectionPage)
 
 const ShopPage = ({ match }) => {
-    const unsubscribeFromSnapshot = null
-    const [loading, setLoading] = useState(true)
+    // const unsubscribeFromSnapshot = null
     const isFetching = useSelector(state => state.shop.isFetching)
+    const isCollectionsLoaded = useSelector(state => !!state.shop.collections)
     const dispatch = useDispatch()
 
     useEffect(() => {
         // Moved to shop reducer using Redux Thunk
-        const collectionRef = firestore.collection('collections')
+        // const collectionRef = firestore.collection('collections')
 
-        collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-            dispatch(updateCollection(collectionsMap))
-            setLoading(false)
-        })
-        return function cleanup () {
-            dispatch(updateCollection(unsubscribeFromSnapshot))
-        }
-        // dispatch(fetchCollectionsStartAsync())
+        // collectionRef.onSnapshot(async snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+        //     dispatch(updateCollection(collectionsMap))
+        //     setLoading(false)
+        // })
+        // return function cleanup () {
+        //     dispatch(updateCollection(unsubscribeFromSnapshot))
+        // }
+        dispatch(fetchCollectionsStartAsync())
     }, [dispatch])
     return (
         <div className='shop-page'>
-            {console.log('this is fetching ', isFetching)}
-            <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading}  {...props} />} />
-            <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionsPageWithSpinner isLoading={loading} {...props} />} />
+            <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={isFetching}  {...props} />} />
+            <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionsPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />} />
         </div>
     )
 }
